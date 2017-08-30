8201269d05f539250032c3be3e81e4e7046ad7ec
angienairovyorjuela <anorjuela7@misena.edu.co>
2017-08-30 16:44:08 -0500

    Salud-virtual0.5

--- Salud Virtual version 0.4/Conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: text/html;charset=utf-8");
-$db= new mysqli('localhost','root','','salud_virtual');
-$acentos=$db->query("SET NAMES 'utf8'");
-if($db->connect_error > 0 ){
-	die('Unable to connect to database [' . $db-> connect_error . ']');
-}
-?>
--- Salud Virtual version 0.4/Salir.php
+++
@@ -1,8 +0,0 @@
-
-<?php 
-session_start();
-
-$_SESSION['estado']="0";
-header('Location: Login.php');
-
-?>
